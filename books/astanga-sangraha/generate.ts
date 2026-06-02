fe59bc951c1d2e6e37f53ac15e9a0ed7beb713bd
eckko <kumar.rahul.ng@gmail.com>
2026-06-02 12:13:56 +0100

    अष्टाङ्गसंग्रह - ch03

--- books/astanga-sangraha/generate.ts
+++ books/astanga-sangraha/generate.ts
@@ -1,14 +1,11 @@
-// import sans from '../txt/ch01.txt';
-// import hindi from '../txt/ch01-hindi.txt';
-
 import {readFileSync, writeFileSync} from 'node:fs';
 import { join } from 'node:path';
 
-const original = readFileSync(join(__dirname, 'ch03.txt'), 'utf8').split(/\n\n\n\n\n/);
-const sandhi = readFileSync(join(__dirname, 'ch03-sandhi.txt'), 'utf8').split(/\n\n\n\n\n/);
-const hindi = readFileSync(join(__dirname, 'ch03-hindi.txt'), 'utf8').split(/\n\n\n\n\n/);
+const original = readFileSync(join(__dirname, 'txt', 'ch03.txt'), 'utf8').split(/\n\n\n\n\n/);
+const sandhi = readFileSync(join(__dirname, 'txt', 'ch03-sandhi.txt'), 'utf8').split(/\n\n\n\n\n/);
+const hindi = readFileSync(join(__dirname, 'txt', 'ch03-hindi.txt'), 'utf8').split(/\n\n\n\n\n/);
 
-const combine = () => {
+const generate = () => {
     console.log(original.length, sandhi.length, hindi.length);
 
     let data = []
@@ -20,11 +17,11 @@ const combine = () => {
         data.push('| संख्या | संस्कृत श्लोक | हिंदी अनुवाद |');
         data.push('|---|---|---|');
         for (let j = 0; j < sandhiArray.length; j++) {
-            const line = `| ${j+1} | **${sandhiArray[j]}** | ${hindiArray[j]} |`;
+            const line = `| 3.${i+1}._${j+1}_ | **${sandhiArray[j]}** | ${hindiArray[j]} |`;
             data.push(line)
         }
-        writeFileSync(join(__dirname, `ch03.${String(i+1).padStart(2, '0')}.md`), data.join('\n'));
+        writeFileSync(join(__dirname, 'md', `ch03.${String(i+1).padStart(2, '0')}.md`), data.join('\n'));
     }
 
 }
-combine()
+generate()
