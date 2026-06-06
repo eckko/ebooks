739bf9e99358ac3ed59b1cd05264b5b9df3566b5
eckko <kumar.rahul.ng@gmail.com>
2026-06-06 22:54:06 +0100

    अष्टाङ्गसंग्रह - ch01

--- books/astanga-sangraha/combine.ts
+++ books/astanga-sangraha/combine.ts
@@ -3,13 +3,13 @@ import { join } from 'node:path';
 
 const combine = () => {
     let data: string[] = []
-    for (let i = 0; i < 40; i++) {
-        const file = readFileSync(join(__dirname, 'txt', `ch01.${String(i + 1).padStart(2, '0')}.txt`), 'utf8');
+    for (let i = 0; i < 12; i++) {
+        const file = readFileSync(join(__dirname, 'txt', `ch02.${String(i + 1).padStart(2, '0')}.txt`), 'utf8');
         console.log(file)
         data = data.concat(file);
         data.push('\n\n\n\n');
     }
-    writeFileSync(join(__dirname, 'txt', `ch01.txt`), data.join('\n'));
+    writeFileSync(join(__dirname, 'txt', `ch02.txt`), data.join('\n'));
 
 }
 combine()
